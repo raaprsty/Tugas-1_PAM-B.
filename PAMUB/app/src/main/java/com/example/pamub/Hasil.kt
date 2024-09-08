41a87f3f9eedb31292acdec5ed9539f0e0f919c8
Rara <raraprasetya15@gmail.com>
2024-09-08 14:28:46 +0700

    Tugas 2 PAM

---
+++ PAMUB/app/src/main/java/com/example/pamub/Hasil.kt
@@ -0,0 +1,23 @@
+package com.example.pamub
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Hasil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_hasil)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val result = intent.getDoubleExtra("RESULT", 0.0)
+        findViewById<TextView>(R.id.textViewResult).text = "Hasil: $result"
+    }
+}
